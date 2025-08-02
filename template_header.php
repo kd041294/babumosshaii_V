ef1390d4f4ff73be49767c6fa250c9536fca1446
kd041294 <kd94.official@gmail.com>
2025-08-03 00:57:59 +0530

    udpated

--- template_header.php
+++ template_header.php
@@ -98,3 +98,13 @@
             }]
         }
     </script>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-L4DKC20V38"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        gtag('config', 'G-L4DKC20V38');
+    </script>
