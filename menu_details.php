bddad72a7a33fe509e3020e733470680e96dad71
kd041294 <kd94.official@gmail.com>
2026-03-23 02:13:56 +0530

    up1

--- menu_details.php
+++ menu_details.php
@@ -226,7 +226,9 @@ $menu = $menuResponse['status'] ? $menuResponse['data'] : null;
         </div>
 
     </div>
-
+    
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script>
         const BASE_URL = "<?= BASE_URL ?>";
 
@@ -249,10 +251,6 @@ $menu = $menuResponse['status'] ? $menuResponse['data'] : null;
             }
         });
     </script>
-
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-
 </body>
 
 </html>
